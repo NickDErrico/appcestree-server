3b00e979f22d070f4cdca8fe09194a941b4ae763
NickDErrico <nickderrico@hotmail.com>
2018-03-12 11:04:19 -0700

    db set up

---
+++ routes/usersRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('../db/knex');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  knex('users')
+    .select()
+    .orderBy('id', 'asc')
+    .then(users => res.json(users))
+})
+
+router.get('/:id', function(req, res) {
+  knex('users')
+    .select()
+    .where('id', req.params.id)
+    .then(user => res.json(user))
+})
+
+router.post('/', function(req, res) {
+  knex('users')
+    .insert(req.body, '*')
+    .then(newUser => res.json(newUser))
+})
+
+router.patch('/:id', function(req, res) {
+  knex('users')
+    .update(req.body)
+    .where('id', req.params.id)
+    .returning('*')
+    .then(updatedUser => res.json(updatedUser))
+})
+
+router.delete('/:id', function(req, res) {
+  knex('users')
+    .del()
+    .where('id', req.params.id)
+    .then(removedUser => removedUser)
+})
+
+module.exports = router
